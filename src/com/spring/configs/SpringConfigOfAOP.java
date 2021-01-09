66ee8d5e8349dba239d50d2c7ab8f615cb5520c9
Edwardwong0826 <edwardwong_94@outlook.com>
2021-01-09 20:45:24 +0800

    AOP Execution and Conclusion
    How AOP is executed, is use CglibAopProxy to get interceptor chain and add advice to  interceptor list, will change advice is that are not interceptor to inteceptor by AdvisorAdapter, if dont have  interceptor chain, invoke target method dicretly. else call l CglibMethodInvocation(proxy, target, method, args, targetClasses, methodProxy).proceed() to trigger interceptor chain mechanism to call advice method and targert method

--- src/com/spring/configs/SpringConfigOfAOP.java
+++ src/com/spring/configs/SpringConfigOfAOP.java
@@ -49,7 +49,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  * ======= above is create and register AnnotationAwareAspectJAutoProxyCreator process ===============
  *          AnnotationJAwareAdvisorAutoProxyCreator => InstantiationAwareBeanPostProcessor
  *
- * AOP execution
+ * AOP Creation
  *  AnnotationAwareAspectJAutoProxyCreator[InstantiationAwareBeanPostProcessor] 作用:
  *  1. in every bean before instantiate, call postProcessBeforeInstantiation()
  *      关心 User and UserProxy creation
@@ -76,6 +76,50 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  *             ObjenesisCglibAopProxy(config); cglib dynamic proxy
  *      4. return to IOC container current component use cglib enhancer dynamic proxy object
  *      5. 以后IOC container get is this component dynamic proxy object, when executed method, dynamic proxy object will execute advice method flow
+ *
+ *  3. method execution
+ *      IOC container save dynamic proxy (cglib enhances object), in this object save all the information(advisor,  target object, xxx)
+ *      1. CglibAopProxy.intercept; - intercept target method execution
+ *      2. according ProxyFactory object obtain 即将执行 target method interceptor chain:
+ *         List<Object> chain = this.advised.getInterceptorAndDynamicInterceptionAdvice(method, targetClass)
+ *         1.List<Object> interceptorList = new ArrayList<Object>(config.getAdvisors.length)
+ *           inside length is 7, 6 from the advice method in User class, one is default ExposeInvocationInterceptor pass by spring
+ *         2.traverse all advisor, change and add to interceptor
+ *           registry.getInterceptors(advisor);
+ *         3.change advisor to List<MethodInterceptor>: - some advisor is not implements MethodInterceptor
+ *           if is MethodInterceptor, direct add to List
+ *           if not, use AdvisorAdapter change advisor to MethodInterceptor
+ *           when change complete, return MethodInterceptor array
+ *
+ *      3. if don't have interceptor chain, invoke target method
+ *         interceptor chain(every advice that is wrapper to MethodInterceptor, use MethodInterceptor mechanism)
+ *      4. if got interceptor chain, pass in execute target Object, target method, interceptor chain to create CglibMethodInvocation() object,
+ *          and call CglibMethodInvocation(proxy, target, method, args, targetClasses, methodProxy).proceed() to return Object type called retVal
+ *          Object retVal = CglibMethodInvocation(xxx).proceed() t
+ *      5. interceptor chain trigger flow
+ *          1. if don't have interceptor direct execute target method, or interceptor index and interceptor array -1 is the same (point to last interceptor) execute target method
+ *          2. chain get every interceptor, interceptor execute invoke() method, every interceptor wait next interceptor run and return only execute
+ *             interceptor chain mechanism, to ensure advice method and target method execution order
+ *
+ *  Conclusion
+ *      1. @EnableAspectJAutoProxy open AOP function
+ *      2. @EnableAspectJAutoProxy will register one component AnnotationAwareAspectJAutoProxyCreator to IOC container
+ *      3. AnnotationAwareAspectJAutoProxyCreator is a BeanPostProcessor
+ *      4. IOC Container Life cycle
+ *          1. register BeanPostProcessors() create AnnotationAwareAspectJAutoProxyCreator object
+ *          2. finishBeanFactoryInitialization() after initialization singleton bean
+ *              1. create logic component and aspect component
+ *              2. AnnotationAwareAspectJAutoProxyCreator will intercept component creation
+ *              3. component after created completed, determine component is it required advice
+ *                  yes: aspect advice method, wrap to Advisor; create logic component proxy dynamic object
+ *      5. target method invoke
+ *          1. dynamic proxy object invoke target method
+ *          2. CglibAopProxy.intercept():
+ *              1. get target method interceptor chain(advisor wrap to interceptor)
+ *              2. use interceptor chain mechanism, 以此进入每一个interceptor to invoke/ execute
+ *              3. result:
+ *                 Normal execution : @Before advice -> target method ->@After advice -> @AfterReturning advice
+ *                 Exception execution: @Before advice -> target method ->@After advice -> @AfterThrowing advice
  *
  */
 
